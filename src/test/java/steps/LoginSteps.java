b4e408ff945e24756acbe3b31c8a896bca88e55d
sebas <sebastianarzadun@gmail.com>
2022-01-18 21:36:31 -0300

    Changes due scenario modification

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,18 +1,19 @@
 package steps;
 
 import io.cucumber.java.en.*;
+import pages.AtlassianPage;
 import pages.LoginPage;
 
 public class LoginSteps {
 
     LoginPage login = new LoginPage();
-
+    AtlassianPage atlassian = new AtlassianPage();
     @Given(": I'm on the login page")
     public void i_m_on_the_login_page() {
         login.navigateToLogin();
     }
-    @When(": I enter my credentials")
-    public void i_enter_my_credentials() {
+    @When(": I enter my mail")
+    public void i_enter_my_mail() {
         login.writeMail();
     }
     @And(": Click on the login button")
@@ -21,9 +22,31 @@ public class LoginSteps {
     login.clickOnLoginButton();
 
     }
+
+    @When(": I enter my password on Atlassian page")
+    public void i_enter_my_password_on_atlassian_page() {
+        atlassian.writePassword();
+
+    }
+
+    @And(": Click on the login submit button")
+    public void click_on_the_login_submit_button() {
+
+        atlassian.clickOnLoginButton();
+
+    }
+
     @Then(": Login is successfully")
     public void login_is_successfully() {
-        System.out.println("Step 4");
+
+        try
+        {
+            Thread.sleep(20000);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
     }
 
 
