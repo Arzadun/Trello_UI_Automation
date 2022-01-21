525dac2bd8f0de876056e0847203ac2244a38c61
sebas <sebastianarzadun@gmail.com>
2022-01-20 22:27:59 -0300

    Added Cucumber reports

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -7,9 +7,14 @@ import org.junit.runner.RunWith;
 import pages.BasePage;
 
 @RunWith(Cucumber.class)
+
 @CucumberOptions(
+        plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "json:target/cucumber-reports.json"
+ },
+
         features = "src/test/resources/Features",
         glue = "steps"
+
         // monochrome = true,
         //tags = "@Test"
 )
