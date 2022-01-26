cfff40594ea4d7f8ca2687656ac4df3a83fc2a48
sebas <sebastianarzadun@gmail.com>
2022-01-26 00:54:24 -0300

    Add background function

--- src/test/java/steps/CardOperationsSteps.java
+++ src/test/java/steps/CardOperationsSteps.java
@@ -1,28 +1,38 @@
 package steps;
 
 import io.cucumber.java.en.*;
+import pages.AtlassianPage;
+import pages.BoardsPage;
+import pages.LoginPage;
 
 public class CardOperationsSteps {
+
+    LoginPage login = new LoginPage();
+    AtlassianPage atlassian = new AtlassianPage();
+    BoardsPage homepage = new BoardsPage();
 
     //Background Steps
 
     @Given("^: I'm on the Trello boards page$")
     public void im_on_the_Trello_boards_page() {
-        System.out.println("holis");
-        throw new io.cucumber.java.PendingException();
+        login.navigateToLogin();
+        login.writeMail();
+        login.clickOnLoginButton();
+        atlassian.writePassword();
+        atlassian.clickOnLoginButton();
+
     }
 
     @When("^: I enter to my workspace$")
     public void i_enter_to_my_workspace() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Holis");
     }
 
 
 
 
 //Scenario: User is able to create new cards on lists
-    @And("^: I click on Add card in a specific list$")
+    @When("^: I click on Add card in a specific list$")
     public void i_click_on_add_card_in_a_specific_list() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
@@ -48,7 +58,7 @@ public class CardOperationsSteps {
 
     //Scenario: User is able to update existing cards on lists
 
-    @And("^: I open the first card in a specific list$")
+    @When("^: I open the first card in a specific list$")
     public void i_open_the_first_card_in_a_specific_list() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
@@ -71,7 +81,7 @@ public class CardOperationsSteps {
 
     //Scenario: User is able to archive cards on lists
 
-    @And("^: I click on the first card in a specific list$")
+    @When("^: I click on the first card in a specific list$")
     public void i_click_on_the_first_card_in_a_specific_list() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
@@ -89,7 +99,7 @@ public class CardOperationsSteps {
 
     //Scenario Outline: User is able to create cards on all lists in board
 
-    @And("^: I click on Add card in a <list> $")
+    @When("^: I click on Add card in a <list> $")
     public void i_click_on_add_card_in_a(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
--- src/test/resources/features/CardOperations.feature
+++ src/test/resources/features/CardOperations.feature
@@ -1,28 +1,28 @@
 Feature: As a user we will be able to perform CRUD operations on cards in board
 
-  Background : I am logged in and enter to my workspace
+  Background:
     Given : I'm on the Trello boards page
     When : I enter to my workspace
 
   Scenario: User is able to create new cards on lists
-    And : I click on Add card in a specific list
+    When : I click on Add card in a specific list
     And : I enter the title of the card
     And : Click on the Add card button
     Then : The card is created
 
   Scenario: User is able to update existing cards on lists
-    And : I open the first card in a specific list
+    When : I open the first card in a specific list
     And : I update the description of the card
     And : Click on save button
     Then : The card is updated
 
   Scenario: User is able to archive cards on lists
-    And : I click on the first card in a specific list
+    When : I click on the first card in a specific list
     And : I click on archive button
     Then : The card is archived
 
   Scenario Outline: User is able to create cards on all lists in board
-    And : I click on Add card in a <list>
+    When : I click on Add card in a <list>
     And : I set the title of the card
     And : I create the card by clicking on the Add card button
     Then : The card is created successfully
